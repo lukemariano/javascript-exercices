0c9dd7e9010e0f68f75bb2332b33236f0b25a9c2
Lucas Mariano <lucas.svmariano@gmail.com>
2022-08-22 10:18:12 -0300

    animação adicionada

--- lista_html_js/wordle_clone/js/script.js
+++ lista_html_js/wordle_clone/js/script.js
@@ -55,6 +55,7 @@ document.addEventListener("keyup", (e) => {
     //captura a tecla do evento e converte para String
     let pressedKey = String(e.key)
 
+
     if (pressedKey === "Backspace" && nextLetter !== 0){
         deleteLetter()
         return
@@ -86,6 +87,7 @@ const insertLetter = (pressedKey) => {
 
     let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
     let box = row.children[nextLetter]
+    animateCSS(box, 'pulse')
     box.textContent = pressedKey
     box.classList.add("filled-box")
     currentGuess.push(pressedKey)
@@ -150,6 +152,8 @@ const checkGuess = () =>{
 
         let delay = 250 * i
         setTimeout(() => {
+            //flip box
+            animateCSS(box, 'flipInX')
             //shade box
             box.style.backgroundColor = letterColor
             shadeKeyBoard(letter, letterColor)
@@ -206,3 +210,23 @@ document.getElementById("keyboard-cont").addEventListener("click", (e) => {
     document.dispatchEvent(new KeyboardEvent("keyup", {'key':key}))
 })
 
+//animation
+
+const animateCSS = (element, animation, prefix = 'animate__') =>
+//Criando uma promise e retornando-a
+    new Promise((resolve, reject) =>{
+        const animationName = `${prefix}${animation}`;
+        //const node = document.querySelector(element)
+        const node = element;
+        node.style.setProperty('--animate-duration', '0.3s');
+
+        node.classList.add(`${prefix}animated`, animationName);
+
+        // Quando a animação termina nos limpamos a classe e resolvemos a promessa
+        function handleAnimationEnd(event){
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`, animationName);
+            resolve('Animation ended');
+        }
+        node.addEventListener('animationend', handleAnimationEnd, {once: true});
+    })
